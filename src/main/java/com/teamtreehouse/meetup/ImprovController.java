dbf1ee902c46ee572c19f9b4be4ea437eb177359
Craig Dennis <craig@teamtreehouse.com>
2014-09-05 16:08:45 -0700

    Working tweets

--- src/main/java/com/teamtreehouse/meetup/ImprovController.java
+++ src/main/java/com/teamtreehouse/meetup/ImprovController.java
@@ -5,6 +5,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.teamtreehouse.meetup.integration.Twitter;
+
 
 @Controller
 public class ImprovController {
@@ -16,7 +18,9 @@ public class ImprovController {
 
 	@RequestMapping("improv")
 	public String improv(@RequestParam(value="name", required=true) String name, Model model) {
+		Twitter twitter = new Twitter();
 		model.addAttribute("name", name);
+		model.addAttribute("tweets", twitter.search(name));
 		return "improv";
 	}
 }
