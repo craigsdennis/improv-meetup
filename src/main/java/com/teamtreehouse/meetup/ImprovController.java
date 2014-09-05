b00027904b7e0d9eafb86890da52b2f80258f9be
Craig Dennis <craig@teamtreehouse.com>
2014-09-05 15:44:45 -0700

    Adds improv page and new dynamic controller

--- src/main/java/com/teamtreehouse/meetup/ImprovController.java
+++ src/main/java/com/teamtreehouse/meetup/ImprovController.java
@@ -3,13 +3,20 @@ package com.teamtreehouse.meetup;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
 public class ImprovController {
 	
-	@RequestMapping
+	@RequestMapping("/")
 	public String index(Model model) {
 		return "index";
+	}
+
+	@RequestMapping("improv")
+	public String improv(@RequestParam(value="name", required=true) String name, Model model) {
+		model.addAttribute("name", name);
+		return "improv";
 	}
 }
