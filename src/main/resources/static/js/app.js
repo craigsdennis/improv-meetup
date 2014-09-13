c988980b40eed49a092dfb04a4a1b89ded1fba75
Craig Dennis <craig@teamtreehouse.com>
2014-09-13 09:31:01 -0700

    Better javascript layout

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,6 +1,49 @@
-$(function() { 
-	var commentRowTemplate = Handlebars.compile($('#commentRowTmpl').html());
-	var loadMoarComments = function() {
+var App = {
+	templates: {},
+	state: {
+		commentsAreSimulating: false,
+	},
+	init: function() {
+		$('script[type="text/x-handlebars-template"]').each(function() {
+			var $script = $(this);
+			App.templates[$script.attr('id')] = Handlebars.compile($script.html());
+		});
+		this.bindEvents();
+	},
+	bindEvents: function() {
+		this.bindCommentEvents();
+		this.bindLearningEvents();
+	},
+	bindCommentEvents: function() {
+		var app = this;
+		$('#commentSimulation').on('click', function(evt) {
+			evt.preventDefault();
+			var $link = $(this);
+			if (app.state.commentsAreSimulating) {
+				app.state.commentsAreSimulating = false;
+				$link.empty();
+				$link.append($('<span />').addClass('glyphicon glyphicon-play'));
+			} else {			
+				app.state.commentsAreSimulating = true;
+				app.simulateComments();
+				$link.empty();
+				$link.append($('<span />').addClass('glyphicon glyphicon-pause'));
+			} 
+		});
+	},
+	bindLearningEvents: function() {
+		$('#learnIncrementer').click(function(evt) {
+			evt.preventDefault();
+			var currentCount = parseInt($('#learningCount').text(), 10);
+			currentCount += 1;
+			$('#learningCount').text(currentCount);
+		});
+		$('#brokenButton').click(function(evt) {
+			console.warn('I warned you this button was broken.');
+			alert(doucment.body.text);
+		});
+	},
+	loadMoarComments: function() {
 		var $comments = $('#comments');
 		var data = $comments.data();
 		var dfd = $.ajax('/api/comments', {
@@ -8,7 +51,7 @@ $(function() {
 		});
 		dfd.then(function(data) {
 			var newComments = _.map(data, function(comment) {
-				return commentRowTemplate(comment);
+				return App.templates.commentRowTmpl(comment);
 			});
 			_.each(newComments, function(comment) {
 				var $comment = $(comment);
@@ -17,29 +60,22 @@ $(function() {
 				$comment.show('slow');
 			});
 		});
-	}
-	var areCommentsSimulating = false;
-	var simulateComments = function() {
+	},
+	simulateComments: function() {
+		var app = this;
 		_.delay(function() {
-			loadMoarComments();
-			// Recurse randomly
-			if (areCommentsSimulating) {				
-				simulateComments();
+			app.loadMoarComments();
+			// Recurse randomly if we are supposed to.
+			console.log('Current simulation state is', app.state.commentsAreSimulating);
+			if (app.state.commentsAreSimulating) {				
+				app.simulateComments();
 			}
 		}, _.random(1500, 4000));
 	
 	}
-	$('#commentSimulation').on('click', function() {
-		$link = $(this); 
-		if (areCommentsSimulating) {
-			areCommentsSimulating = false;
-			$link.empty();
-			$link.append($('<span />').addClass('glyphicon glyphicon-play'));
-		} else {			
-			areCommentsSimulating = true;
-			simulateComments();
-			$link.empty();
-			$link.append($('<span />').addClass('glyphicon glyphicon-pause'));
-		} 
-	});
+};
+
+
+$(function() {
+	App.init();
 });
